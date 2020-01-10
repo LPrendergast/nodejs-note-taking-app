2eaf3cb202854c8b2f9a7e0a916fe9b708c4abf1
Luke Prendergast <luke.prendergast94@gmail.com>
2020-01-10 17:52:24 +0000

    Created foundations for note taking app. All backend JS

---
+++ app.js
@@ -0,0 +1,53 @@
+const validator = require("validator");
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+const fs = require("fs");
+
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      description: "Note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Removing the note.");
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "list notes",
+  handler: () => {
+    console.log("Listing notes.");
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: () => {
+    console.log("Reading a note.");
+  }
+});
+
+console.log(yargs.argv);
